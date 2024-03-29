f6a23cd854ad2b85ae4225ac946b382f4958637f
aquapi <aquaplmc@gmail.com>
2024-03-30 00:07:37 +0700

    Initial

---
+++ dev.ts
@@ -0,0 +1,32 @@
+import { Byte, send } from '@bit-js/byte';
+import { Glob } from 'bun';
+
+const publicDir = `${import.meta.dir}/public/`;
+const dirLen = publicDir.length;
+
+const app = new Byte();
+
+// Serve html
+const paths = new Glob('**/*.html').scanSync({
+    absolute: true,
+    onlyFiles: true,
+    cwd: publicDir
+});
+
+for (const path of paths) app.any(
+    path.endsWith('index.html')
+        ? path.substring(dirLen, path.length - 10)
+        : path.substring(dirLen, path.length - 5),
+
+    () => send.body(Bun.file(path))
+);
+
+
+// Serve assets
+app.fallback((ctx) => send.body(Bun.file(publicDir + ctx.path)));
+
+// Serve with Bun
+Bun.serve({
+    fetch: app.fetch,
+    error: () => new Response()
+});
